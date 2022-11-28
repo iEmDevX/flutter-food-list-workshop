778641ececbe8f130f93789770c7ad85f73f35be
Watcharapong Sribun <watcharapong_sribun@outlook.com>
2022-11-28 18:39:54 +0700

    update IntrinsicHeight of row

--- lib/widgets/food_card_widget.dart
+++ lib/widgets/food_card_widget.dart
@@ -19,83 +19,84 @@ class FoodCardWidget extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Container(
-        height: 100,
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(15),
           color: const Color(0xFFEFEEEE),
         ),
-        child: Row(
-          children: [
-            ClipRRect(
-              borderRadius: BorderRadius.circular(16.0),
-              child: Image.asset(
-                foodInfo.image,
-                width: 100,
-                height: 100,
+        child: IntrinsicHeight(
+          child: Row(
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(16.0),
+                child: Image.asset(
+                  foodInfo.image,
+                  width: 100,
+                  height: 100,
+                ),
               ),
-            ),
-            const SizedBox(width: 8),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
+              const SizedBox(width: 8),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Text(
+                      foodInfo.name,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                    ),
+                    Text(foodInfo.price.toString()),
+                  ],
+                ),
+              ),
+              Column(
                 children: [
-                  Text(
-                    foodInfo.name,
-                    style: const TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16,
+                  GestureDetector(
+                    onTap: () {
+                      foodInfo.increaseAmount();
+                      onTabIncrease();
+                    },
+                    child: Container(
+                      decoration: const BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(15),
+                        ),
+                        color: Color(0xFF91ECA5),
+                      ),
+                      height: 50,
+                      width: 50,
+                      child: const Icon(
+                        Icons.add,
+                        color: Colors.white,
+                      ),
                     ),
                   ),
-                  Text(foodInfo.price.toString()),
+                  GestureDetector(
+                    onTap: () {
+                      foodInfo.decreaseAmount();
+                      onTabDecrease();
+                    },
+                    child: Container(
+                      decoration: const BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                          bottomRight: Radius.circular(15),
+                        ),
+                        color: Color.fromRGBO(237, 155, 155, 1),
+                      ),
+                      height: 50,
+                      width: 50,
+                      child: const Icon(
+                        Icons.remove,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
                 ],
               ),
-            ),
-            Column(
-              children: [
-                GestureDetector(
-                  onTap: () {
-                    foodInfo.increaseAmount();
-                    onTabIncrease();
-                  },
-                  child: Container(
-                    decoration: const BoxDecoration(
-                      borderRadius: BorderRadius.only(
-                        topRight: Radius.circular(15),
-                      ),
-                      color: Color(0xFF91ECA5),
-                    ),
-                    height: 50,
-                    width: 50,
-                    child: const Icon(
-                      Icons.add,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-                GestureDetector(
-                  onTap: () {
-                    foodInfo.decreaseAmount();
-                    onTabDecrease();
-                  },
-                  child: Container(
-                    decoration: const BoxDecoration(
-                      borderRadius: BorderRadius.only(
-                        bottomRight: Radius.circular(15),
-                      ),
-                      color: Color.fromRGBO(237, 155, 155, 1),
-                    ),
-                    height: 50,
-                    width: 50,
-                    child: const Icon(
-                      Icons.remove,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
